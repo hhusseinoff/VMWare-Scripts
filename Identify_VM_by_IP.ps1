3d577e1953d0cd907c8213da84d858bcd6165a56
hhusseinoff <h.husseinoff@outlook.com>
2023-11-23 15:26:00 +0200

    Initial Commit

---
+++ Identify_VM_by_IP.ps1
@@ -0,0 +1,19 @@
+﻿# Connect to vCenter
+$vcServer = "Name.com"
+Connect-VIServer -Server $vcServer
+
+# Search for VM with specified IP address
+$targetIP = "1.2.3.4"
+$vmWithTargetIP = Get-VM | Where-Object {
+    $_.Guest.IPAddress -contains $targetIP
+}
+
+# Display the VM name
+if ($vmWithTargetIP) {
+    Write-Host "The VM with IP address $targetIP is: $($vmWithTargetIP.Name)"
+} else {
+    Write-Host "No VM found with IP address $targetIP"
+}
+
+# Disconnect from vCenter
+Disconnect-VIServer -Server $vcServer -Confirm:$false
---
+++ PowerCLI_ListVMsAndIPs.ps1
@@ -0,0 +1,34 @@
+﻿# Connect to vCenter
+$vcServer = "name.com"
+Connect-VIServer -Server $vcServer
+
+# Option to export results to CSV
+$exportToCSV = $true
+$csvPath = "C:\Temp\VMWare_VM_IPList_Nov9th.csv"
+
+# Retrieve VM data
+$vmData = Get-VM | ForEach-Object {
+    $vm = $_
+    $vm.Guest.Nics | ForEach-Object {
+        $ipv4Addresses = ($_.IPAddress | Where-Object { $_ -match '^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}$' }) -join ', '
+        $ipv6Addresses = ($_.IPAddress | Where-Object { $_ -notmatch '^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}$' }) -join ', '
+        [PSCustomObject]@{
+            'VM Name'     = $vm.Name
+            'IPv4 Address'= $ipv4Addresses
+            'IPv6 Address'= $ipv6Addresses
+            'MAC Address' = $_.MacAddress
+        }
+    }
+}
+
+# Display the data as a table
+$vmData | Format-Table -AutoSize
+
+# Export to CSV if required
+if ($exportToCSV) {
+    $vmData | Export-Csv -Path $csvPath -NoTypeInformation
+    Write-Host "Data exported to $csvPath"
+}
+
+# Disconnect from vCenter
+Disconnect-VIServer -Server $vcServer -Confirm:$false
